6bae51b3361d00d3e34567296370d4d20f85c884
Vivek Pandya <vivekvpandya@gmail.com>
2014-05-17 23:16:53 +0530

    route changed to /user/verify

--- lib/Drupal/uauth/Controller/UauthController.php
+++ lib/Drupal/uauth/Controller/UauthController.php
@@ -4,6 +4,8 @@ namespace Drupal\uauth\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class UauthController extends ControllerBase {
 
@@ -15,13 +17,25 @@ class UauthController extends ControllerBase {
 
    public function uauthPage(){
 
-     $build = array(
+     /*$build = array(
        '#type' => 'markup',
        '#markup' => t('Hello World'),
      );
      return $build;
+*/
+$account = $this->currentUser();
+$userName = $account->getUsername();
+$email = $account->getEmail();
+$roles = $account->getRoles();
+$responseArray = array('name' => $userName,
+                        'email' => $email,
+                        'roles' => $roles
+
+);
+$response = new Response(json_encode($responseArray));
+$response->headers->set('Content-Type','application/json');
 
 
-
+return $response;
    }
 }
